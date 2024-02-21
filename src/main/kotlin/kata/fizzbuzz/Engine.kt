ed83c2e2ba0b92e6f3e2b7f726a24c7575b5c58f
Sam Cranford <me@nostradamnit.com>
2024-02-21 21:48:02 +0100

    Now we're talking! IsFizz is telling us something!

--- src/main/kotlin/kata/fizzbuzz/Engine.kt
+++ src/main/kotlin/kata/fizzbuzz/Engine.kt
@@ -2,13 +2,17 @@ package kata.fizzbuzz
 
 class Engine {
     fun say(number: Int): String {
-        if (isMultipleOf(3, number)) {
+        if (isFizz(number)) {
             return "Fizz"
         }
         if (number % 5 == 0) {
             return "Buzz"
         }
         return number.toString()
+    }
+
+    private fun isFizz(number: Int): Boolean {
+        return isMultipleOf(3, number)
     }
 
     private fun isMultipleOf(divisor: Int, number: Int): Boolean{
