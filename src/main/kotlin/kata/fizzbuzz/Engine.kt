98de659ce5cc23d559064fae10d2766326043a57
Sam Cranford <me@nostradamnit.com>
2024-02-21 21:18:39 +0100

    First failing test - can't say "1"

---
+++ src/main/kotlin/kata/fizzbuzz/Engine.kt
@@ -0,0 +1,8 @@
+package kata.fizzbuzz
+
+class Engine {
+    fun say(i: Int): String {
+        return ""
+    }
+
+}
--- src/test/kotlin/kata/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/kata/fizzbuzz/FizzBuzzTest.kt
@@ -1,10 +1,14 @@
 package kata.fizzbuzz
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class FizzBuzzTest {
     @Test
     fun testCanSayOne() {
-
+        val engine = Engine()
+        val one = engine.say(1)
+        assertEquals("1", one)
     }
 
 }
