c7140cf9ad314ef3693c6a3debf3b0e0723a4f76
Sam Cranford <me@nostradamnit.com>
2024-02-21 21:28:33 +0100

    make Engine a class variable to remove duplication, especially as it requires no setup.

--- src/test/kotlin/kata/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/kata/fizzbuzz/FizzBuzzTest.kt
@@ -4,16 +4,15 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class FizzBuzzTest {
+    private val engine = Engine()
     @Test
     fun testCanSayOne() {
-        val engine = Engine()
         val one = engine.say(1)
         assertEquals("1", one)
     }
 
     @Test
     fun testCanSayFizz() {
-        val engine = Engine()
         val fizz = engine.say(3)
         assertEquals("Fizz", fizz)
     }
